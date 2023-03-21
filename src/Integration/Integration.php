0356785a0a3d36ae801e2808482c91f87dfaa661
Julian Krzefski <krzefski@heptacom.de>
2023-03-21 10:26:13 +0000

    Mute deprecations and user-deprecations

--- src/Integration/Integration.php
+++ src/Integration/Integration.php
@@ -7,10 +7,23 @@ namespace HeptaConnect\Production\Integration;
 use Heptacom\HeptaConnect\Bridge\ShopwarePlatform\AbstractIntegration;
 use HeptaConnect\Production\Integration\DependencyInjection\CompilerPass\AutoloadPortals;
 use HeptaConnect\Production\Integration\DependencyInjection\CompilerPass\RemoveObstructiveServices;
+use Psr\Log\NullLogger;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\ErrorHandler\ErrorHandler;
 
 final class Integration extends AbstractIntegration
 {
+    public function boot(): void
+    {
+        parent::boot();
+
+        $logger = new NullLogger();
+        ErrorHandler::register(null, false)->setLoggers([
+            \E_DEPRECATED => $logger,
+            \E_USER_DEPRECATED => $logger,
+        ]);
+    }
+
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);

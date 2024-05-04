97802605f24462b91296eb252206e3480ffead1c
Julian Krzefski <krzefski@heptacom.de>
2024-05-04 14:27:20 +0200

    Install symfony/runtime

--- public/index.php
+++ public/index.php
@@ -4,51 +4,28 @@ declare(strict_types=1);
 
 use HeptaConnect\Production\DevOps\Kernel\HttpKernel;
 use Shopware\Core\Framework\Plugin\KernelPluginLoader\ComposerPluginLoader;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
-if (\PHP_VERSION_ID < 70403) {
-    \header('Content-type: text/html; charset=utf-8', true, 503);
+$_SERVER['SCRIPT_FILENAME'] = __FILE__;
+$projectRoot = \dirname(__DIR__);
 
-    echo '<h2>Error</h2>';
-    echo 'Your server is running PHP version ' . \PHP_VERSION . ' but HEPTAconnect requires at least PHP 7.4.3';
-    exit(1);
+if (
+    !\file_exists($projectRoot . '/.env')
+    && !\file_exists($projectRoot . '/.env.dist')
+    && !\file_exists($projectRoot . '/.env.local.php')
+) {
+    $_SERVER['APP_RUNTIME_OPTIONS']['disable_dotenv'] = true;
 }
 
+require_once __DIR__ . '/../vendor/autoload_runtime.php';
 $classLoader = require __DIR__ . '/../vendor/autoload.php';
 
-$projectRoot = \dirname(__DIR__);
-if (\class_exists(Dotenv::class) && (\file_exists($projectRoot . '/.env.local.php') || \file_exists($projectRoot . '/.env') || \file_exists($projectRoot . '/.env.dist'))) {
-    (new Dotenv())->usePutenv()->bootEnv(\dirname(__DIR__) . '/.env');
-}
+return function (array $context) use ($classLoader): HttpKernelInterface {
+    $appEnv = $context['APP_ENV'] ?? 'prod';
+    $debug = (bool) ($context['APP_DEBUG'] ?? ($appEnv !== 'prod'));
 
-$appEnv = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
-$debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? ($appEnv !== 'prod'));
+    $httpKernel = new HttpKernel($appEnv, $debug, $classLoader);
+    $httpKernel->setPluginLoader(new ComposerPluginLoader($classLoader));
 
-if ($debug) {
-    \umask(0000);
-
-    Debug::enable();
-}
-
-$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
-if ($trustedProxies) {
-    Request::setTrustedProxies(\explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
-}
-
-$trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false;
-if ($trustedHosts) {
-    Request::setTrustedHosts(\explode(',', $trustedHosts));
-}
-
-$request = Request::createFromGlobals();
-
-$kernel = new HttpKernel($appEnv, $debug, $classLoader);
-$kernel->setPluginLoader(new ComposerPluginLoader($classLoader));
-
-$result = $kernel->handle($request);
-
-$result->getResponse()->send();
-
-$kernel->terminate($result->getRequest(), $result->getResponse());
+    return $httpKernel->getKernel();
+};

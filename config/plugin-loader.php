295a9bd24c2753975c214b3b332c53d7c2a8cdbe
Julian Krzefski <krzefski@heptacom.de>
2024-06-04 13:12:42 +0200

    Change package type to `project` in composer.json

---
+++ config/plugin-loader.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HeptaConnect\Production;
+
+use Composer\Autoload\ClassLoader;
+use Composer\Factory;
+use Composer\IO\NullIO;
+use HeptaConnect\Production\Integration\Integration;
+use Shopware\Core\Framework\Plugin\KernelPluginLoader\KernelPluginLoader;
+use Shopware\Core\Framework\Plugin\KernelPluginLoader\StaticKernelPluginLoader;
+
+if (\function_exists('HeptaConnect\Production\getPluginLoader')) {
+    return;
+}
+
+function getPluginLoader(string $projectRoot, ClassLoader $classLoader): KernelPluginLoader
+{
+    $integrationClass = new \ReflectionClass(Integration::class);
+    $composer = (new Factory())->createComposer(new NullIO(), $projectRoot . '/composer.json');
+    $rootPackage = $composer->getPackage();
+
+    return new StaticKernelPluginLoader($classLoader, null, [
+        [
+            'name' => $integrationClass->getShortName(),
+            'baseClass' => $integrationClass->getName(),
+            'active' => true,
+            'path' => $projectRoot,
+            'version' => $rootPackage->getVersion(),
+            'autoload' => $rootPackage->getAutoload(),
+            'managedByComposer' => true,
+            'composerName' => $rootPackage->getName(),
+        ],
+    ]);
+}
--- public/index.php
+++ public/index.php
@@ -3,8 +3,9 @@
 declare(strict_types=1);
 
 use HeptaConnect\Production\DevOps\Kernel\HttpKernel;
-use Shopware\Core\Framework\Plugin\KernelPluginLoader\ComposerPluginLoader;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+use function HeptaConnect\Production\getPluginLoader;
 
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 $projectRoot = \dirname(__DIR__);
@@ -19,13 +20,14 @@ if (
 
 require_once __DIR__ . '/../vendor/autoload_runtime.php';
 $classLoader = require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config/plugin-loader.php';
 
-return function (array $context) use ($classLoader): HttpKernelInterface {
+return function (array $context) use ($classLoader, $projectRoot): HttpKernelInterface {
     $appEnv = $context['APP_ENV'] ?? 'prod';
     $debug = (bool) ($context['APP_DEBUG'] ?? ($appEnv !== 'prod'));
 
     $httpKernel = new HttpKernel($appEnv, $debug, $classLoader);
-    $httpKernel->setPluginLoader(new ComposerPluginLoader($classLoader));
+    $httpKernel->setPluginLoader(getPluginLoader($projectRoot, $classLoader));
 
     return $httpKernel->getKernel();
 };
